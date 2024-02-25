705227ee27dedeadee312de1aa406d14a44f98bb
superegonico <150907717+superegonico@users.noreply.github.com>
2024-02-25 15:31:12 +0100

    initial commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import "./style.css";
+import Lenis from "@studio-freight/lenis";
+import { gsap } from "gsap/dist/gsap";
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
+
+const lenis = new Lenis();
+
+lenis.on("scroll", ScrollTrigger.update);
+
+gsap.ticker.add((time) => {
+  lenis.raf(time * 1000);
+});
+
+gsap.ticker.lagSmoothing(0);
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./dist/*.html", "./src/index.js"],
+  theme: {
+    extend: {
+      fontFamily: {
+        ibm: ["IBM Plex Sans", "sans-serif"],
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+};
