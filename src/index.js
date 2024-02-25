8f61400f0acf47f2684878fb0bf1932353b98f8b
superegonico <150907717+superegonico@users.noreply.github.com>
2024-02-25 23:27:18 +0100

    ...

--- src/index.js
+++ src/index.js
@@ -7,10 +7,42 @@ gsap.registerPlugin(ScrollTrigger);
 
 const lenis = new Lenis();
 
-lenis.on("scroll", ScrollTrigger.update);
+document.querySelectorAll(".players__player").forEach((node, index) => {
+  node.style.setProperty("--index", index % 4);
+});
+
+lenis.on("scroll", () => {
+  document.body.style.setProperty(
+    "--scroll",
+    window.scrollY / (document.body.offsetHeight - window.innerHeight)
+  );
+});
 
 gsap.ticker.add((time) => {
   lenis.raf(time * 1000);
 });
 
 gsap.ticker.lagSmoothing(0);
+
+const targets = document.querySelectorAll(".section img");
+
+const lazyLoad = (target) => {
+  const io = new IntersectionObserver((entries, observer) => {
+    entries.forEach((entry) => {
+      console.log("😍");
+
+      if (entry.isIntersecting) {
+        console.log("image loaded");
+        const img = entry.target;
+        const src = img.getAttribute("data-lazy");
+
+        img.setAttribute("src", src);
+        observer.disconnect();
+      }
+    });
+  });
+
+  io.observe(target);
+};
+
+targets.forEach(lazyLoad);
