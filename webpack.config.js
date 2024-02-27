19b285599a47245f5f936b011ec633b50adbb855
superegonico <150907717+superegonico@users.noreply.github.com>
2024-02-27 22:42:44 +0100

    template html

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   mode: "development",
@@ -30,6 +31,9 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/template.html",
+    }),
+    new CopyWebpackPlugin({
+      patterns: [{ from: "./src/assets/images", to: "images" }],
     }),
   ],
 };
